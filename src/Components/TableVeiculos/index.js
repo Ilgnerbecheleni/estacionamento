fcda5ec9163c58ac620d57d17282178c67d1162b
ilgner becheleni <ilgner.gui@gmail.com>
2023-02-16 11:08:38 -0300

    tabelas

--- src/Components/TableVeiculos/index.js
+++ src/Components/TableVeiculos/index.js
@@ -1,1 +1,31 @@
+ import React from 'react'
+ import styles from './tableveiculo.module.css'
+ export default function TableVeiculos() {
+   return (
+    <div>
+    <table className={styles.tabela}>
+    <thead>
+        <td>Placa</td>
+        <td>Cliente</td>
+        <td>Tipo</td>
+        <td>Marca</td>
+        <td>Modelo</td>
+        <td>Cor</td>
+    </thead>
+    <tr key="veiculo">
+        <td>FFZ5455</td>
+        <td>Ilgner</td>
+        <td>Carro</td>
+        <td>GM</td>
+        <td>Celta</td>
+        <td>Prata</td>
+    </tr>
+
+  </table>
+
+
+
+</div>
+   )
+ }
  
--- src/Paginas/Veiculos/index.js
+++ src/Paginas/Veiculos/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './veiculos.module.css'
+import BotaoAdicionar from '../../Components/BotaoAdicionar'
+import TableVeiculos from '../../Components/TableVeiculos'
 export default function Veiculos() {
   return (
     <div className={styles.veiculos}>
       <h1>Veiculos</h1>
+      <BotaoAdicionar/>
+      <TableVeiculos/>
     </div>
   )
 }
